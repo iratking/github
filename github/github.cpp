e5924c10ea89263780ce26fcf354da16591e3ab6
al <al@COLLAPSAR>
2022-12-20 18:30:01 +0300

    Добавьте файлы проекта.

---
+++ github/github.cpp
@@ -0,0 +1,11 @@
+﻿// github.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
