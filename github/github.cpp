0662b8b683b914ca4660631bdaa32d263e600be4
Yao Ling <linggyao@outlook.com>
2022-12-20 19:14:21 +0300

    Убрал

--- github/github.cpp
+++ github/github.cpp
@@ -1,12 +1,12 @@
-﻿// github.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World\n";
+    std::cout << "Hello World!\n";
     std::cout << "laba last =)";
+    std::cout << "ddddd!";
 }
 
 
